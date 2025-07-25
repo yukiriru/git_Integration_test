40b83021b59d983a0e5c3468285ba8589cd59eb2
yukiriru <yukiriru1222@gmail.com>
2025-07-25 10:20:43 +0900

    곱셈추가

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ public class Calculator {
     public static void main(String[] args){
         System.out.println("1+1=2");
         System.out.println("1-1=0");
+        System.out.println("1-1=0");
         System.out.println("2*4=8");
     }
 }
